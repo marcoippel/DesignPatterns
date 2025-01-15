3949523135c506e9ce3c35c705962b7db55b6112
Marco Ippel <marco@ippelsoft.nl>
2025-01-15 13:41:16 +0100

    files verplaatst

--- DecoratorPattern/Business/CoffeeDecorator.cs
+++ DecoratorPattern/Business/CoffeeDecorator.cs
@@ -1,11 +1,10 @@
-﻿using DecoratorPattern.Business;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DecoratorPattern.Decorators
+namespace DecoratorPattern.Business
 {
     public abstract class CoffeeDecorator : ICoffee
     {
